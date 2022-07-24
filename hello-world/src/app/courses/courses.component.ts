2dc5705c4e728a7c16d179c0caae0fe13d63f0e0
Tenghy23 <66905172+Tenghy23@users.noreply.github.com>
2022-07-24 18:00:43 +0800

    directive example

--- hello-world/src/app/courses/courses.component.ts
+++ hello-world/src/app/courses/courses.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+/* @Component is called a decorator function */
 @Component({
   selector: 'courses',
   template: `
     <h2>{{ getTitle() }}<h2>
     <ul>
+      <!-- DOM refers to object/nodes that represent the page -->
+      <!-- directives are used to manipulate the DOM -->
+      <!-- when u modify the DOM w a directive, append * before the ng method -->
       <li *ngFor="let course of courses">
+          <!-- string interpolation uses dbl curly bracket -->
           {{ course }}
       </li>
     </ul>
     `,
   styleUrls: ['./courses.component.scss']
 })
+
+/* pascal Naming convention, this is how we declare
+a class */
 export class CoursesComponent implements OnInit {
 
   constructor() { }
